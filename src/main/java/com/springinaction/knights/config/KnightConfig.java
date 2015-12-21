df1e5e2118290b825aeefd010c9676f42cdf597d
Mark Wait <mwait@gmail.com>
2015-12-21 10:23:40 +0100

    Dodanie aspektu, konfiguracja w xml i Java

--- src/main/java/com/springinaction/knights/config/KnightConfig.java
+++ src/main/java/com/springinaction/knights/config/KnightConfig.java
@@ -2,6 +2,7 @@ package com.springinaction.knights.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 import com.springinaction.knights.BraveKnight;
 import com.springinaction.knights.Knight;
@@ -9,6 +10,7 @@ import com.springinaction.knights.Quest;
 import com.springinaction.knights.SlayDragonQuest;
 
 @Configuration
+@ImportResource("classpath:knights-config.xml") 
 public class KnightConfig {
 
 	@Bean
